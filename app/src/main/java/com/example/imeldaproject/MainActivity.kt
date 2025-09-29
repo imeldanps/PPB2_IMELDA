28b94117e9f1f71b3c6e625145507eedc5038521
imeldanps <imeldanadilaps29@gmail.com>
2025-09-29 13:35:18 +0700

    init

---
+++ app/src/main/java/com/example/imeldaproject/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.imeldaproject
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.trusted.Token
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.exceptions.NoCredentialException
+import androidx.lifecycle.lifecycleScope
+import com.example.imeldaproject.databinding.ActivityMainBinding
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.oAuthCredential
+import kotlinx.coroutines.launch
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var credentialManager: CredentialManager
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        credentialManager = CredentialManager.create(this)
+        auth = Firebase.auth
+
+        registerEvents()
+    }
+
+    fun registerEvents() {
+        binding.btnLogin.setOnClickListener {
+            lifecycleScope.launch {
+                val request = prepareRequest()
+                loginByGoogle(request)
+            }
+            Log.d("imelda", "button di click")
+        }
+
+    }
+
+    fun prepareRequest(): GetCredentialRequest {
+        val serverClientId =
+            "1012986818895-f47rb0umsjl2rs6i572ot68ssgdtaehr.apps.googleusercontent.com"
+
+        val googleOption = GetGoogleIdOption
+            .Builder()
+            .setFilterByAuthorizedAccounts(false)
+            .setServerClientId(serverClientId)
+            .build()
+
+        val request = GetCredentialRequest
+            .Builder()
+            .addCredentialOption(googleOption)
+            .build()
+
+        return request
+    }
+
+    suspend fun loginByGoogle(request: GetCredentialRequest) {
+        try {
+            val result = credentialManager.getCredential(
+                context = this,
+                request = request
+            )
+
+            val credential = result.credential
+            val idToken = GoogleIdTokenCredential.createFrom(credential.data)
+
+            firebaseLoginCallback(idToken.idToken)
+
+        } catch (exc: NoCredentialException) {
+            Toast.makeText(this, "Login gagal :" + exc.message, Toast.LENGTH_LONG).show()
+        } catch (exc: Exception) {
+            Toast.makeText(this, "Login gagal :" + exc.message, Toast.LENGTH_LONG).show()
+        }
+    }
+    suspend fun LoginByGoogle(request: GetCredentialRequest) {
+
+    }
+    fun firebaseLoginCallback(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) {task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Login berhasil :", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Login gagal:", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+}
